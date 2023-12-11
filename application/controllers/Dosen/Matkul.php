e4cfd78f36c5b5682add82f606c2b219893c353d
vjrmy <vicjeremyp@gmail.com>
2023-12-11 22:57:55 +0700

    still datatables

--- application/controllers/Dosen/Matkul.php
+++ application/controllers/Dosen/Matkul.php
@@ -1,13 +1,16 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Matkul extends CI_Controller{
-  function __construct(){
+class Matkul extends CI_Controller
+{
+  function __construct()
+  {
     parent::__construct();
     $this->load->model('M_matkul'); //load model M_matkul
   }
-  public function index(){
-    $data=array(
+  public function index()
+  {
+    $data = array(
       'nama' => $this->session->userdata('ses_nama'),
       'hari'     => $this->M_matkul->get_hari(),
       'jrs'     => $this->M_matkul->get_jurus(),
@@ -15,52 +18,62 @@ class Matkul extends CI_Controller{
       'akses' => $this->session->userdata('akses')
     );
     $this->load->view('template_admin/header');
-    $this->load->view('template_admin/navbar',$data);
-    $this->load->view('v_dosen/v_matkul',$data);
+    $this->load->view('template_admin/navbar', $data);
+    $this->load->view('v_dosen/v_matkul', $data);
     $this->load->view('template_admin/footer');
-
   }
 
-  public function getData() { 
-    $result = $this->M_matkul->getDataTables();
+  public function getData()
+  {
+    $results = $this->M_matkul->getDataTables();
+    $data =[];
+    $no = $_POST['start'];
+    foreach ($results as $result){
+      $row = array();
+      $row[] = ++$no;
+      $row[] = $result->
+    }
   }
-
-  function simpan(){ //function simpan data
-    $data=array(
-      // 'id' => $this->input->post('id'),
-      'periode' => $this->input->post('periode'),
-      'jrs' => $this->input->post('jrs'),
-      'matkul' => $this->input->post('matkul'),
-      'semester' => $this->input->post('semester'),
-      'ruang' => $this->input->post('ruang'),
-      'hari' => $this->input->post('hari'),
-      'waktu' => $this->input->post('waktu'),
-    );
-    $this->db->insert('tbl_matkul', $data);
-    redirect('Matkul');
-  }
- 
-  function update(){ //function update data
-    $kode=$this->input->post('id');
-    $data=array(
-      'periode' => $this->input->post('periode'),
-      'jrs' => $this->input->post('jrs'),
-      'matkul' => $this->input->post('matkul'),
-      'semester' => $this->input->post('semester'),
-      'ruang' => $this->input->post('ruang'),
-      'hari' => $this->input->post('hari'),
-      'waktu' => $this->input->post('waktu'),
-    );
-    $this->db->where('id',$kode);
-    $this->db->update('tbl_matkul', $data);
-    redirect('Matkul');
-  }
- 
-  function delete(){ //function hapus data
-    $kode=$this->input->post('mk_id');
-    $this->db->where('mk_id',$kode);
-    $this->db->delete('tbl_matkul');
-    redirect('Matkul');
-  }
- 
 }
+
+//   function simpan()
+//   { //function simpan data
+//     $data = array(
+//       // 'id' => $this->input->post('id'),
+//       'periode' => $this->input->post('periode'),
+//       'jrs' => $this->input->post('jrs'),
+//       'matkul' => $this->input->post('matkul'),
+//       'semester' => $this->input->post('semester'),
+//       'ruang' => $this->input->post('ruang'),
+//       'hari' => $this->input->post('hari'),
+//       'waktu' => $this->input->post('waktu'),
+//     );
+//     $this->db->insert('tbl_matkul', $data);
+//     redirect('Matkul');
+//   }
+
+//   function update()
+//   { //function update data
+//     $kode = $this->input->post('id');
+//     $data = array(
+//       'periode' => $this->input->post('periode'),
+//       'jrs' => $this->input->post('jrs'),
+//       'matkul' => $this->input->post('matkul'),
+//       'semester' => $this->input->post('semester'),
+//       'ruang' => $this->input->post('ruang'),
+//       'hari' => $this->input->post('hari'),
+//       'waktu' => $this->input->post('waktu'),
+//     );
+//     $this->db->where('id', $kode);
+//     $this->db->update('tbl_matkul', $data);
+//     redirect('Matkul');
+//   }
+
+//   function delete()
+//   { //function hapus data
+//     $kode = $this->input->post('mk_id');
+//     $this->db->where('mk_id', $kode);
+//     $this->db->delete('tbl_matkul');
+//     redirect('Matkul');
+//   }
+// }
--- application/models/M_matkul.php
+++ application/models/M_matkul.php
@@ -36,18 +36,36 @@ class M_matkul extends CI_Model{
     }
   }
   public function getDataTables(){
+    $this->_get_data_query();
 
     $query = $this->db->get();
+    return $query->result();
+  }
+  public function count_filtered_data(){
+    $this->_get_data_query();
+
+    $query = $this->db->get();
+    return $query->num_rows();
+  }
+  public function count_all_data(){
+    // $this->db->from($this->table);
+    $this->db->select('*');
+    $this->db->from('tbl_matkul');
+    $this->db->join('tbl_periode', 'periode=per_id');
+    $this->db->join('tbl_jurusan', 'jrs=jrs_id');
+    $this->db->join('tbl_hari', 'hari=hari_id'); 
+    $this->db->add_column('view', '<a href="javascript:void(0);" class="edit_record btn btn-info btn-xs" data-kode="$1" data-nama="$2" data-harga="$3" data-kategori="$4">Edit</a>  <a href="javascript:void(0);" class="hapus_record btn btn-danger btn-xs" data-kode="$1">Hapus</a>','barang_kode,barang_nama,barang_harga,kategori_id,kategori_nama');
+    return $this->db->count_all_results();
   }
   
 
-  function get_matkul() { 
-    $this->datatables->select('*');
-    $this->datatables->from('tbl_matkul');
-    $this->datatables->join('tbl_periode', 'periode=per_id');
-    $this->datatables->join('tbl_jurusan', 'jrs=jrs_id');
-    $this->datatables->join('tbl_hari', 'hari=hari_id'); 
-    $this->datatables->add_column('view', '<a href="javascript:void(0);" class="edit_record btn btn-info btn-xs" data-kode="$1" data-nama="$2" data-harga="$3" data-kategori="$4">Edit</a>  <a href="javascript:void(0);" class="hapus_record btn btn-danger btn-xs" data-kode="$1">Hapus</a>','barang_kode,barang_nama,barang_harga,kategori_id,kategori_nama');
-    return $this->datatables->generate();
-  }
+  // function get_matkul() { 
+  //   $this->db->select('*');
+  //   $this->db->from('tbl_matkul');
+  //   $this->db->join('tbl_periode', 'periode=per_id');
+  //   $this->db->join('tbl_jurusan', 'jrs=jrs_id');
+  //   $this->db->join('tbl_hari', 'hari=hari_id'); 
+  //   $this->db->add_column('view', '<a href="javascript:void(0);" class="edit_record btn btn-info btn-xs" data-kode="$1" data-nama="$2" data-harga="$3" data-kategori="$4">Edit</a>  <a href="javascript:void(0);" class="hapus_record btn btn-danger btn-xs" data-kode="$1">Hapus</a>','barang_kode,barang_nama,barang_harga,kategori_id,kategori_nama');
+  //   return $this->datatables->generate();
+  // }
 }
